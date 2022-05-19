f2db24f0b3e5d7ae7322bbe4cce62e2a4710f134
Julietanilem <Julietanilem@gmail.com>
2022-05-18 20:13:48 -0500

    Avence tablero :(

--- dynamics/js/Tablero.js
+++ dynamics/js/Tablero.js
@@ -1,9 +1,12 @@
 const tabla= document.getElementById("tablero");
 const btn_puntaje = document.getElementById("btn-puntaje");
 const btn_nivel = document.getElementById("btn-nivel");
+const audioExplo= new Audio ("./statics/media/audio/vanish_1.mp3");
 let dificultad=0;
 const matriz=[]; 
-matriz[0]=[0,0]; 
+matriz[0]=[0,0];
+var click=true;
+var ganaste=0; 
 function crearTablero(filas, minas)
 {
     matriz[0]=[0,0]; 
@@ -16,10 +19,14 @@ function crearTablero(filas, minas)
 
     class Casilla 
     {
-        constructor (tieneMina,minasCerca, tienebandera){
+        constructor (tieneMina,minasCerca, tienebandera, volteado, ident, i, ii){
             this.tieneMina=tieneMina;
             this.minasCerca=minasCerca;
             this.tienebandera=tienebandera;
+            this.volteado=volteado;
+            this.ident=ident;
+            this.i=i;
+            this.ii=ii;
         };
     };
     let linea="";
@@ -30,11 +37,13 @@ function crearTablero(filas, minas)
         ii=0;
         while(ii<filas)
         {
-           matriz[i][ii]= new Casilla (false, 0, false);
-         
+            linea+="<td class='casillita' id='"+i+ii+"'></td>"
+            var pal=i.toString();
+            var z=ii.toString();
+            var q=pal+z;
+
+            matriz[i][ii]= new Casilla (false, 0, false, false, q, i, ii);
             ii++;
-         
-            linea+="<td id='"+i+ii+"'>+</td>"
         }
         linea+="</tr>";
    
@@ -58,18 +67,93 @@ function bombas(cantidad, filas, matriz)
     }
   
 }; 
-function asigNum (){
-    ejex=0;
-    ejey=0;
+function asigNum (matriz, filas){
+    var ejex=0;
+    var ejey=0;
+    var  suma=0;
+   while(ejex<filas)
+   {
+       ejey=0;
+       suma=0;
+       while(ejey<filas)
+       {
+           suma=0;
+            if(ejex-1>=0)
+            {
+                if(matriz[ejex-1][ejey].tieneMina==true)
+                {
+                    suma++;
+                }
+                if(ejey-1>=0)
+                {
+                    if(matriz[ejex-1][ejey-1].tieneMina==true)
+                    {
+                        suma++;
+                    } 
+                }
+                if(ejey+1<filas)
+                {
+                    if(matriz[ejex-1][ejey+1].tieneMina==true)
+                    {
+                        suma++;
+                    }
+                }
+                
+            }
+            if(ejex+1<filas){
+                if(matriz[ejex+1][ejey].tieneMina==true)
+                {
+                    suma++;
+                }
+                if(ejey-1>=0)
+                {
+                    if(matriz[ejex+1][ejey-1].tieneMina==true)
+                    {
+                        suma++;
+                    }
+                }
+                if(ejey+1<filas)
+                {
+                  if(matriz[ejex+1][ejey+1].tieneMina==true)
+                    {
+                        suma++;
+                    }  
+                }
+                
+            }
+
+            if(ejey-1>=0)
+            {
+                if(matriz[ejex][ejey-1].tieneMina==true)
+                {
+                    suma++;
+                }
+            }
+            if(ejey+1<filas)
+            {
+                if(matriz[ejex][ejey+1].tieneMina==true)
+                {
+                    suma++;
+                }
+            }
+            matriz[ejex][ejey].minasCerca=suma;
+           // console.log(matriz[ejex][ejey]);
+           suma=0;
+            ejey++;
+       }
+        ejex++;
+   }
+       
+   
 
 }
-function limpiar(){
-    //abrir casillas
+function limpiar(matriz, target, fila, col){
+    
 }
 
 btn_nivel.addEventListener("click", (evento) =>{
     dificultad = evento.target.id; 
-    
+    console.log(evento.target);
     if(dificultad == "f")
     {
 
@@ -91,67 +175,123 @@ btn_nivel.addEventListener("click", (evento) =>{
 
     btn_nivel.style.display="none"; 
 });
-var click=0;
-var ganaste=0;
+var volteados;
 tabla.addEventListener("click", (evento)=>
 {
-    if(ganaste==1)
+    if(evento.target.id!="tablero")
     {
-        alert("ganaste");
-        //redirijirte a registr
-    }
-    console.log(evento);
-    cuadid=evento.target.id;
-    click++;
-    var fila=0;
-    var col=0;
-    if(click=1)
-    {
-        if(dificultad == "f") 
+        if(ganaste==1)
         {
-            var filas=8;
-            var bombas1=10;
-            bombas(10, 8, matriz); 
+            alert("ganaste");
+            //redirijirte a registr
+        }
+        cuadid=evento.target.id;
+        
+        var fila=0;
+        var col=0;
+        if(dificultad == "f"  ) 
+        {
             fila=cuadid/10;
             fila=Math.floor(fila);
-             col=cuadid%10;
+            col=cuadid%10;
+            var filas=8;
+            var bombas1=10;
         }
-            
-        if(dificultad == "m") 
+        console.log(cuadid);
+        if(dificultad == "d" || dificultad == "m" ) 
         {
-            const  filas1=10;
-            const bombas1=40;
-             bombas(40,10, matriz); 
+            if(cuadid<100)
+            {
+                fila=cuadid/10;
+                fila=Math.floor(fila);
+                col=cuadid%10;
+                var filas=24;
+                var bombas1=40;
+            }
+            else if (cuadid<1000){
+                fila=cuadid/100;
+                fila=Math.floor(fila);
+                col=cuadid%10;
+                var filas=10;
+                var bombas1=40;
+            }
+            else if(cuadid<10000)
+            {
+                fila=cuadid/100;
+                fila=Math.floor(fila);
+                col=cuadid%100;
+                const  filas=24;
+                const bombas1=90;
+            }
         }
+        
+        console.log(fila+" "+col);
+        if(click==true)
+        {
+            if(dificultad == "f") 
+            {
             
+                bombas(10, 8, matriz); 
+        
+            }
+                
+            if(dificultad == "m") 
+            {
+                
+                 bombas(40,16, matriz); 
+            }
+                
+        
+            if(dificultad == "d")
+            {
+               
+                 bombas(90, 24, matriz); 
+            }
+               
+          asigNum(matriz, filas);
+        }
     
-        if(dificultad == "d")
-        {
-            const  filas1=24;
-            const bombas1=90;
-             bombas(90, 24, matriz); 
+       
+        if(matriz[fila][col].tieneMina==true && click==false)
+            {
+                evento.target.innerHTML="<img src='./statics/img/mina.jpg' id='img'/ >"
+                audioExplo.addEventListener("ended", ()=>{
+                        alert("Perdiste :((");
+                });
+               
+                // window.location.href = "."
+            }
+        else{
+            evento.target.innerHTML=matriz[fila][col].minasCerca;
+            matriz[fila][col].volteado=true;
+            evento.target.setAttribute("style", "background-color:pink" );
+            volteados++;
+            limpiar(matriz, evento.target.id, fila, col);
         }
-           
-      asigNum();
+        if(click== (filas*filas)-bombas1){
+            ganaste=1;
+        }
+        click=false;
+
     }
-  
-    console.log(matriz[fila][col]);
-    if(matriz[fila][col].tieneMina==true)
-    {
-        alert("Perdiste :((");
-        //redirijirte a inicio
-    }
-    else{
-        limpiar();
-    }
-    if(click== (filas*filas)-bombas1){
-        ganaste=1;
-    }
- 
+   
+   
     
 });
 tabla.addEventListener('contextmenu', (evento)=>{
-    evento.target.style.color="red";
-    evento.target.tienebandera=true;
+    if(   evento.target.tienebandera==true)
+    {
+        evento.target.tienebandera=false;
+        evento.target.setAttribute("style", "background-color:brown");
+        evento.target.innerHTML=" "
+    }
+    else{
+        evento.target.tienebandera=true;
+        evento.target.setAttribute("style", "background-color:white");
+        evento.target.innerHTML="<img src='./statics/img/bandera.png' id='img' >"
+   
+    }
+     evento.preventDefault();
+    
     
 });
